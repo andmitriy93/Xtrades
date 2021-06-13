e311db1c5ed80578e29465d18705df09c6cfd99d
Dmitrii An <an.dmitriy93@gmail.com>
2021-06-13 14:25:13 -0500

    Initial setup

---
+++ src/components/dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './sidebar'
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div>
+        <Sidebar />
+        
+      </div>
+    )
+  }
+}
+
+export default Dashboard
