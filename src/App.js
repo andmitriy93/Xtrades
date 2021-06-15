ae2b707bc787b3fdd53b53c657b1931413052cad
Dmitrii An <an.dmitriy93@gmail.com>
2021-06-14 21:35:07 -0500

    changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from './components/dashboard/dashboard';
-import User from './components/user_page/user_show_page';
+// import User from './components/user_page/user_show_page';
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
         <Switch>
-          <Route exact path='/dashboard' component={Dashboard} />
-          <Route exact path="/user" component={User} />
+          <Route exact path='/' component={Dashboard} />
+          {/* <Route exact path="/user" component={User} /> */}
         </Switch>
       </BrowserRouter>
     </div>
